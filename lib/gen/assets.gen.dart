8dba48b8a89195d09eb21d6931028f07b4819b79
mugri-ndeh <frunwindeh@gmail.com>
2025-11-12 18:07:39 +0100

    auto generated items

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,380 @@
+// dart format width=80
+
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart' as _svg;
+import 'package:vector_graphics/vector_graphics.dart' as _vg;
+
+class $AssetsFontsGen {
+  const $AssetsFontsGen();
+
+  /// File path: assets/fonts/OpenSans-Bold.ttf
+  String get openSansBold => 'assets/fonts/OpenSans-Bold.ttf';
+
+  /// File path: assets/fonts/OpenSans-ExtraBold.ttf
+  String get openSansExtraBold => 'assets/fonts/OpenSans-ExtraBold.ttf';
+
+  /// File path: assets/fonts/OpenSans-Light.ttf
+  String get openSansLight => 'assets/fonts/OpenSans-Light.ttf';
+
+  /// File path: assets/fonts/OpenSans-Medium.ttf
+  String get openSansMedium => 'assets/fonts/OpenSans-Medium.ttf';
+
+  /// File path: assets/fonts/OpenSans-Regular.ttf
+  String get openSansRegular => 'assets/fonts/OpenSans-Regular.ttf';
+
+  /// File path: assets/fonts/OpenSans-SemiBold.ttf
+  String get openSansSemiBold => 'assets/fonts/OpenSans-SemiBold.ttf';
+
+  /// File path: assets/fonts/Roboto-Black.ttf
+  String get robotoBlack => 'assets/fonts/Roboto-Black.ttf';
+
+  /// File path: assets/fonts/Roboto-Bold.ttf
+  String get robotoBold => 'assets/fonts/Roboto-Bold.ttf';
+
+  /// File path: assets/fonts/Roboto-ExtraBold.ttf
+  String get robotoExtraBold => 'assets/fonts/Roboto-ExtraBold.ttf';
+
+  /// File path: assets/fonts/Roboto-ExtraLight.ttf
+  String get robotoExtraLight => 'assets/fonts/Roboto-ExtraLight.ttf';
+
+  /// File path: assets/fonts/Roboto-Light.ttf
+  String get robotoLight => 'assets/fonts/Roboto-Light.ttf';
+
+  /// File path: assets/fonts/Roboto-Medium.ttf
+  String get robotoMedium => 'assets/fonts/Roboto-Medium.ttf';
+
+  /// File path: assets/fonts/Roboto-Regular.ttf
+  String get robotoRegular => 'assets/fonts/Roboto-Regular.ttf';
+
+  /// File path: assets/fonts/Roboto-SemiBold.ttf
+  String get robotoSemiBold => 'assets/fonts/Roboto-SemiBold.ttf';
+
+  /// File path: assets/fonts/Roboto-Thin.ttf
+  String get robotoThin => 'assets/fonts/Roboto-Thin.ttf';
+
+  /// List of all assets
+  List<String> get values => [
+    openSansBold,
+    openSansExtraBold,
+    openSansLight,
+    openSansMedium,
+    openSansRegular,
+    openSansSemiBold,
+    robotoBlack,
+    robotoBold,
+    robotoExtraBold,
+    robotoExtraLight,
+    robotoLight,
+    robotoMedium,
+    robotoRegular,
+    robotoSemiBold,
+    robotoThin,
+  ];
+}
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/Arrow Left-Outline.svg
+  SvgGenImage get arrowLeftOutline =>
+      const SvgGenImage('assets/icons/Arrow Left-Outline.svg');
+
+  /// File path: assets/icons/Buy.svg
+  SvgGenImage get buy => const SvgGenImage('assets/icons/Buy.svg');
+
+  /// File path: assets/icons/Calendar.svg
+  SvgGenImage get calendar => const SvgGenImage('assets/icons/Calendar.svg');
+
+  /// File path: assets/icons/Cart-Fill.svg
+  SvgGenImage get cartFill => const SvgGenImage('assets/icons/Cart-Fill.svg');
+
+  /// File path: assets/icons/Chat.svg
+  SvgGenImage get chat => const SvgGenImage('assets/icons/Chat.svg');
+
+  /// File path: assets/icons/Document.svg
+  SvgGenImage get document => const SvgGenImage('assets/icons/Document.svg');
+
+  /// File path: assets/icons/Email-Bulk.svg
+  SvgGenImage get emailBulk => const SvgGenImage('assets/icons/Email-Bulk.svg');
+
+  /// File path: assets/icons/Fire.svg
+  SvgGenImage get fire => const SvgGenImage('assets/icons/Fire.svg');
+
+  /// File path: assets/icons/GPS-Fill.svg
+  SvgGenImage get gPSFill => const SvgGenImage('assets/icons/GPS-Fill.svg');
+
+  /// File path: assets/icons/Group.svg
+  SvgGenImage get group => const SvgGenImage('assets/icons/Group.svg');
+
+  /// File path: assets/icons/Home.svg
+  SvgGenImage get home => const SvgGenImage('assets/icons/Home.svg');
+
+  /// File path: assets/icons/Location.svg
+  SvgGenImage get location => const SvgGenImage('assets/icons/Location.svg');
+
+  /// File path: assets/icons/Minus.svg
+  SvgGenImage get minus => const SvgGenImage('assets/icons/Minus.svg');
+
+  /// File path: assets/icons/Notification.svg
+  SvgGenImage get notification =>
+      const SvgGenImage('assets/icons/Notification.svg');
+
+  /// File path: assets/icons/Password-Outline.svg
+  SvgGenImage get passwordOutline =>
+      const SvgGenImage('assets/icons/Password-Outline.svg');
+
+  /// File path: assets/icons/Phone-Fill.svg
+  SvgGenImage get phoneFill => const SvgGenImage('assets/icons/Phone-Fill.svg');
+
+  /// File path: assets/icons/Phone-Outline.svg
+  SvgGenImage get phoneOutline =>
+      const SvgGenImage('assets/icons/Phone-Outline.svg');
+
+  /// File path: assets/icons/Plus.svg
+  SvgGenImage get plus => const SvgGenImage('assets/icons/Plus.svg');
+
+  /// File path: assets/icons/payment.svg
+  SvgGenImage get payment => const SvgGenImage('assets/icons/payment.svg');
+
+  /// List of all assets
+  List<SvgGenImage> get values => [
+    arrowLeftOutline,
+    buy,
+    calendar,
+    cartFill,
+    chat,
+    document,
+    emailBulk,
+    fire,
+    gPSFill,
+    group,
+    home,
+    location,
+    minus,
+    notification,
+    passwordOutline,
+    phoneFill,
+    phoneOutline,
+    plus,
+    payment,
+  ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/apple.png
+  AssetGenImage get apple => const AssetGenImage('assets/images/apple.png');
+
+  /// File path: assets/images/google.png
+  AssetGenImage get google => const AssetGenImage('assets/images/google.png');
+
+  /// File path: assets/images/logo.png
+  AssetGenImage get logo => const AssetGenImage('assets/images/logo.png');
+
+  /// File path: assets/images/onboard-step1.png
+  AssetGenImage get onboardStep1 =>
+      const AssetGenImage('assets/images/onboard-step1.png');
+
+  /// File path: assets/images/onboard-step2.png
+  AssetGenImage get onboardStep2 =>
+      const AssetGenImage('assets/images/onboard-step2.png');
+
+  /// File path: assets/images/onboard-step3.png
+  AssetGenImage get onboardStep3 =>
+      const AssetGenImage('assets/images/onboard-step3.png');
+
+  /// File path: assets/images/success.png
+  AssetGenImage get success => const AssetGenImage('assets/images/success.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+    apple,
+    google,
+    logo,
+    onboardStep1,
+    onboardStep2,
+    onboardStep3,
+    success,
+  ];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsFontsGen fonts = $AssetsFontsGen();
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+    this.animation,
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+  final AssetGenImageAnimation? animation;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({AssetBundle? bundle, String? package}) {
+    return AssetImage(_assetName, bundle: bundle, package: package);
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class AssetGenImageAnimation {
+  const AssetGenImageAnimation({
+    required this.isAnimation,
+    required this.duration,
+    required this.frames,
+  });
+
+  final bool isAnimation;
+  final Duration duration;
+  final int frames;
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName, {this.size, this.flavors = const {}})
+    : _isVecFormat = false;
+
+  const SvgGenImage.vec(this._assetName, {this.size, this.flavors = const {}})
+    : _isVecFormat = true;
+
+  final String _assetName;
+  final Size? size;
+  final Set<String> flavors;
+  final bool _isVecFormat;
+
+  _svg.SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    String? package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    _svg.SvgTheme? theme,
+    _svg.ColorMapper? colorMapper,
+    ColorFilter? colorFilter,
+    Clip clipBehavior = Clip.hardEdge,
+    @deprecated Color? color,
+    @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
+    @deprecated bool cacheColorFilter = false,
+  }) {
+    final _svg.BytesLoader loader;
+    if (_isVecFormat) {
+      loader = _vg.AssetBytesLoader(
+        _assetName,
+        assetBundle: bundle,
+        packageName: package,
+      );
+    } else {
+      loader = _svg.SvgAssetLoader(
+        _assetName,
+        assetBundle: bundle,
+        packageName: package,
+        theme: theme,
+        colorMapper: colorMapper,
+      );
+    }
+    return _svg.SvgPicture(
+      loader,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      colorFilter:
+          colorFilter ??
+          (color == null ? null : ColorFilter.mode(color, colorBlendMode)),
+      clipBehavior: clipBehavior,
+      cacheColorFilter: cacheColorFilter,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
